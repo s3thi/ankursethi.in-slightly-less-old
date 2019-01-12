701f3e4b53167843ea9a00d0525948f470bde0b3
Ankur Sethi <contact@ankursethi.in>
2019-01-12 19:05:41 +0530

    Add gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: "Ankur Sethi is Alive and Well",
     siteUrl: "https://ankursethi.in"
-  }
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: `${__dirname}/content/`
+      }
+    }
+  ]
 };
