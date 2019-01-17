ddfa2d8993a2448d3ca91deab93a1d26929d42b6
Ankur Sethi <contact@ankursethi.in>
2019-01-17 14:02:16 +0530

    Build pages and posts using separate logic

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,25 +1,49 @@
-const path = require('path');
+const path = require("path");
+const moment = require("moment");
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-
-  const result = await graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              slug
-            }
+const markdownQuery = (regex) => `
+  {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] },
+      filter: { fileAbsolutePath:{ regex: "${regex}" } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            slug
+            date
           }
         }
       }
     }
-  `);
+  }
+`;
 
-  for (let edge of result.data.allMarkdownRemark.edges) {
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const pageRegex = (s => s.raw)`/(\/content\/pages)/.*\\.md$/`;
+  const pageResult = await graphql(markdownQuery(pageRegex));
+
+  for (let edge of pageResult.data.allMarkdownRemark.edges) {
     createPage({
       path: edge.node.frontmatter.slug,
+      component: path.resolve("./src/templates/page.js"),
+      context: {
+        slug: edge.node.frontmatter.slug
+      }
+    });
+  }
+
+  const postRegex = (s => s.raw)`/(\/content\/posts)/.*\\.md$/`;
+  const postResult = await graphql(markdownQuery(postRegex));
+
+  for (let edge of postResult.data.allMarkdownRemark.edges) {
+    const date = moment(edge.node.frontmatter.date);
+    const slug = edge.node.frontmatter.slug;
+    const postPath = `${date.year()}/${date.month() + 1}/${date.date()}/${slug}`;
+    createPage({
+      path: postPath,
       component: path.resolve("./src/templates/page.js"),
       context: {
         slug: edge.node.frontmatter.slug
