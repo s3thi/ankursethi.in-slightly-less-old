f8d84b0642a979b707beb4b3cc1bc3eba34bfcca
Ankur Sethi <contact@ankursethi.in>
2019-01-17 16:26:06 +0530

    Add social links

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,21 @@
 module.exports = {
   siteMetadata: {
     title: "Ankur Sethi is Alive and Well",
-    siteUrl: "https://ankursethi.in"
+    url: "https://ankursethi.in",
+    socialLinks: [
+      {
+        service: "Twitter",
+        url: "https://twitter.com/ankurs3thi"
+      },
+      {
+        service: "Medium",
+        url: "https://medium.com/@ankurs3thi"
+      },
+      {
+        service: "GitHub",
+        url: "https://github.com/s3thi"
+      }
+    ]
   },
   plugins: [
     {
