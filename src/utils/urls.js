9cb3a0a0ae5dd0a2f10cacd4012d7ad28346b232
Ankur Sethi <contact@ankursethi.in>
2019-01-18 17:26:28 +0530

    Add function for generating slugs from category names

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,6 +1,10 @@
 const moment = require("moment");
+const slugify = require("slugify");
 
 module.exports.makePostUrl = (date, slug) => {
   const d = moment(date);
   return `/${d.year()}/${d.month() + 1}/${d.date()}/${slug}`;
 };
+
+module.exports.makeCategoryUrl = category =>
+  `/category/${slugify(category, { lower: true })}/`;
